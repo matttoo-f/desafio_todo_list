8f0bc13d1c8db68e6624a072cea2b718ecbaf765
matttoo-f <mato.arttattoo@gmail.com>
2024-01-20 04:30:35 -0300

    modificaciones generales,50% completado

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,49 +1,88 @@
-// Elementos del DOM
-const tareaInput = document.getElementById("tareaInput");
-const agregarBtn = document.getElementById("agregar");
-const tareasList = document.getElementById("tareas");
-const borrarLista = document.getElementById("borrarLista")
-const cantidadTotal = document.getElementById("total")
-const cantidadRealizada = document.getElementById("realizadas")
+// Elementos del DOM------------------------------->
 
-let tareas = [{id: Date.now(), tarea: tareaInput.value, completado: false}]
+const input = document.getElementById("input")
+const addBtn = document.getElementById("add")
+const total = document.getElementById("total")
+const pending = document.getElementById("pendientes")
+const ready = document.getElementById("realizadas")
+const taskList = document.getElementById("listaTareas")
+
+// variables---------------------------------------->
+
+let taskArray = [{id: Date.now(), task: "Respasar guía de estudios", ready: true},
+                 {id: Date.now(), task: "Comenzar desafío", ready: false},
+                 {id: Date.now(), task: "finalizar desafío", ready: false},
+]
 
 
-function contarTareas() {
-    const totalTareas = tareas.length;
-    cantidadTotal.innerHTML = totalTareas;
+// eventos------------------------------------------>
 
-}
-
-function borrar(id) {
-    const index = tareas.findIndex((ele) => ele.id == id);
-    tareas.splice(index, 1);
-
-    let tareaIngresada = "";
-    for (let tarea of tareas) {
-        tareaIngresada += `<li>${tarea.tarea}<button onclick="borrar(${tarea.id})"> x </button> </li>`;
+document.addEventListener("DOMContentLoaded",()=> {
+    let html = ""
+    for (let task of taskArray) {
+        let taskClass = task.ready ? 'ready' : 'pending'
+        html += `<tr class="${taskClass}"><td>${task.id}</td><td>${task.task}</td><td><button onclick="btnReady(${task.id})">Ready</button></td><td><button onclick="borrar(${task.id})">X</button></td></tr>`    
     }
-    tareasList.innerHTML = tareaIngresada;
-    contarTareas()
-}
-
-// Eventos
-agregarBtn.addEventListener("click", () => {
-    const nuevaTarea = { id: Date.now(), tarea: tareaInput.value, completado: false };
-    tareas.push(nuevaTarea);
-    tareaInput.value = "";
-
-    let tareaIngresada = "";
-    for (let tarea of tareas) {
-        tareaIngresada += `<li>${tarea.tarea} <button onclick="borrar(${tarea.id})">x</button></li>`;
-    }
-    tareasList.innerHTML = tareaIngresada;
-    contarTareas()
-});
-
-
-borrarLista.addEventListener ("click", function(){
-    tareasList.innerHTML = ""
-    tareas = []
-    contarTareas()
+    taskList.innerHTML = html
+    contar()
 })
+
+addBtn.addEventListener("click",()=> {
+    const newTask = input.value
+    taskArray.push({id: Date.now(), task: newTask, ready: false})
+    input.value = ""
+
+    let html = ""
+    for(let task of taskArray){
+        let taskClass = task.ready ? 'ready' : 'pending'
+        html += `<tr class="${taskClass}"><td>${task.id}</td><td>${task.task}</td><td><button onclick="btnReady(${task.id})">Ready</button></td><td><button onclick="borrar(${task.id})">X</button></td></tr>`    
+    }
+    taskList.innerHTML = html
+    if (newTask == ""){
+        html =""
+    }
+    contar()
+})
+
+// Funciones----------------------------------------->
+
+function borrar (id){
+    const index = taskArray.findIndex((ele) => ele.id == id )
+    taskArray.splice(index, 1)
+
+    let html = ""
+    for(let task of taskArray){
+        let taskClass = task.ready ? 'ready' : 'pending'
+        html += `<tr class="${taskClass}"><td>${task.id}</td><td>${task.task}</td><td><button onclick="btnReady(${task.id})">Ready</button></td><td><button onclick="borrar(${task.id})">X</button></td></tr>`    
+    }
+    taskList.innerHTML = html
+    contar()
+}
+
+
+
+function contar(){
+    let count = taskArray.length
+    total.innerHTML = count
+
+    let completedTasks = taskArray.filter(task => task.ready).length;
+    let pendingTasks = count - completedTasks;
+
+    ready.innerHTML = completedTasks;
+    pending.innerHTML = pendingTasks;
+}
+
+function btnReady(id){
+    const index = taskArray.findIndex((ele)=> ele.id == id)
+    if (index !== -1 && !taskArray[index].ready){
+        taskArray[index].ready = true
+        
+        let html = "";
+        for (let task of taskArray) {
+            let taskClass = task.ready ? 'ready' : 'pending';
+            html += `<tr class="${taskClass}"><td>${task.id}</td><td>${task.task}</td><td><button onclick="btnReady(${task.id})">Ready</button></td><td><button onclick="borrar(${task.id})">X</button></td></tr>`;
+        }
+        taskList.innerHTML = html
+    }
+    contar()
+}
